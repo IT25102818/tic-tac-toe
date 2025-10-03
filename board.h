10929e82daf71d54e17a1e35a63bde6794b329c8
Sarthani JA <it25102818@my.sliit.lk>
2025-10-03 16:12:47 +0530

    board.h

--- board.h
+++ board.h
@@ -1,15 +1,16 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#include "types.h"               // use Game
+#include "types.h"               
 
-char **allocBoard(int n);        // allocate n x n
-void freeBoard(char **b, int n); // free memory
-void clearBoard(Game *g);        // fill with '.'
+char **allocBoard(int n);        
+void freeBoard(char **b, int n); 
+void clearBoard(Game *g);        
 
-void printBoard(const Game *g);              // show board
-void openLog(Game *g, const char *filename); // open append log
-void closeLog(Game *g);                      // close log
-void logBoard(const Game *g);                // write board to log
+void printBoard(const Game *g);              
+void openLog(Game *g, const char *filename); 
+void closeLog(Game *g);                      
+void logBoard(const Game *g);                
 
 #endif // BOARD_H
+
